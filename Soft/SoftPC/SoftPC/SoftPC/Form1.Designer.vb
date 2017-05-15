9fee5d90d174d55527cf34bdc0f2773b6f6fb960
iotincho <martin.barrera@outlook.com>
2017-05-15 19:35:11 -0300

    Primera entrega completo, con software para la pc, y asm completo(falta antirebote) comunicacion totalemte implementada

---
+++ Soft/SoftPC/SoftPC/SoftPC/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.txtRecibido = New System.Windows.Forms.TextBox()
+        Me.lblEstado = New System.Windows.Forms.Label()
+        Me.btnBuscarPuertos = New System.Windows.Forms.Button()
+        Me.cmbPuertos = New System.Windows.Forms.ComboBox()
+        Me.btnConectar = New System.Windows.Forms.Button()
+        Me.btnDesconectar = New System.Windows.Forms.Button()
+        Me.SPPuerto = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtArchivo = New System.Windows.Forms.TextBox()
+        Me.lstTiempos = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'txtRecibido
+        '
+        Me.txtRecibido.Location = New System.Drawing.Point(222, 42)
+        Me.txtRecibido.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtRecibido.Multiline = True
+        Me.txtRecibido.Name = "txtRecibido"
+        Me.txtRecibido.Size = New System.Drawing.Size(189, 312)
+        Me.txtRecibido.TabIndex = 22
+        '
+        'lblEstado
+        '
+        Me.lblEstado.AutoSize = True
+        Me.lblEstado.Location = New System.Drawing.Point(438, 65)
+        Me.lblEstado.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblEstado.Name = "lblEstado"
+        Me.lblEstado.Size = New System.Drawing.Size(71, 13)
+        Me.lblEstado.TabIndex = 21
+        Me.lblEstado.Text = "Desconectdo"
+        '
+        'btnBuscarPuertos
+        '
+        Me.btnBuscarPuertos.Location = New System.Drawing.Point(440, 96)
+        Me.btnBuscarPuertos.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnBuscarPuertos.Name = "btnBuscarPuertos"
+        Me.btnBuscarPuertos.Size = New System.Drawing.Size(120, 23)
+        Me.btnBuscarPuertos.TabIndex = 20
+        Me.btnBuscarPuertos.Text = "Buscar Puertos"
+        Me.btnBuscarPuertos.UseVisualStyleBackColor = True
+        '
+        'cmbPuertos
+        '
+        Me.cmbPuertos.FormattingEnabled = True
+        Me.cmbPuertos.Location = New System.Drawing.Point(440, 42)
+        Me.cmbPuertos.Name = "cmbPuertos"
+        Me.cmbPuertos.Size = New System.Drawing.Size(121, 21)
+        Me.cmbPuertos.TabIndex = 18
+        '
+        'btnConectar
+        '
+        Me.btnConectar.Location = New System.Drawing.Point(440, 124)
+        Me.btnConectar.Name = "btnConectar"
+        Me.btnConectar.Size = New System.Drawing.Size(120, 23)
+        Me.btnConectar.TabIndex = 17
+        Me.btnConectar.Text = "CONECTAR"
+        Me.btnConectar.UseVisualStyleBackColor = True
+        '
+        'btnDesconectar
+        '
+        Me.btnDesconectar.Location = New System.Drawing.Point(440, 154)
+        Me.btnDesconectar.Name = "btnDesconectar"
+        Me.btnDesconectar.Size = New System.Drawing.Size(120, 23)
+        Me.btnDesconectar.TabIndex = 16
+        Me.btnDesconectar.Text = "DESCONECTAR"
+        Me.btnDesconectar.UseVisualStyleBackColor = True
+        '
+        'SPPuerto
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 398)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 24
+        Me.Label1.Text = "Archivo"
+        '
+        'txtArchivo
+        '
+        Me.txtArchivo.Location = New System.Drawing.Point(21, 414)
+        Me.txtArchivo.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtArchivo.Name = "txtArchivo"
+        Me.txtArchivo.Size = New System.Drawing.Size(539, 20)
+        Me.txtArchivo.TabIndex = 23
+        Me.txtArchivo.Text = "C:\Users\Tincho\Desktop\prueva"
+        '
+        'lstTiempos
+        '
+        Me.lstTiempos.FormattingEnabled = True
+        Me.lstTiempos.Location = New System.Drawing.Point(19, 39)
+        Me.lstTiempos.Name = "lstTiempos"
+        Me.lstTiempos.Size = New System.Drawing.Size(135, 303)
+        Me.lstTiempos.TabIndex = 25
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(599, 460)
+        Me.Controls.Add(Me.lstTiempos)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtArchivo)
+        Me.Controls.Add(Me.txtRecibido)
+        Me.Controls.Add(Me.lblEstado)
+        Me.Controls.Add(Me.btnBuscarPuertos)
+        Me.Controls.Add(Me.cmbPuertos)
+        Me.Controls.Add(Me.btnConectar)
+        Me.Controls.Add(Me.btnDesconectar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtRecibido As System.Windows.Forms.TextBox
+    Friend WithEvents lblEstado As System.Windows.Forms.Label
+    Friend WithEvents btnBuscarPuertos As System.Windows.Forms.Button
+    Friend WithEvents cmbPuertos As System.Windows.Forms.ComboBox
+    Friend WithEvents btnConectar As System.Windows.Forms.Button
+    Friend WithEvents btnDesconectar As System.Windows.Forms.Button
+    Friend WithEvents SPPuerto As System.IO.Ports.SerialPort
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtArchivo As System.Windows.Forms.TextBox
+    Friend WithEvents lstTiempos As System.Windows.Forms.ListBox
+
+End Class
---
+++ Soft/SoftPC/SoftPC/SoftPC/Form1.vb
@@ -0,0 +1,125 @@
+﻿Public Class Form1
+
+
+    Private Sub btnBuscarPuertos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBuscarPuertos.Click
+        Try
+            cmbPuertos.Items.Clear()
+
+            For Each puerto As String In My.Computer.Ports.SerialPortNames
+                cmbPuertos.Items.Add(puerto)
+                cmbPuertos.SelectedIndex() = 0
+            Next
+
+            If cmbPuertos.Items.Count = 0 Then
+                MsgBox("No hay puertos", MsgBoxStyle.Critical)
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub btnConectar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnConectar.Click
+        Try
+            With SPPuerto
+                .BaudRate = 9600
+                .Parity = IO.Ports.Parity.None
+                .DataBits = 8
+                .StopBits = 1
+                .PortName = cmbPuertos.SelectedItem.ToString()
+                .Open()
+
+                If .IsOpen Then
+                    lblEstado.Text = "Conectado"
+                Else
+                    MsgBox("Error de conexion", MsgBoxStyle.Critical)
+                End If
+            End With
+
+        Catch ex As Exception
+            MsgBox("Error de conexion", MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Private Sub btnDesconectar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDesconectar.Click
+        SPPuerto.Close()
+        lblEstado.Text = "Desconectado"
+    End Sub
+
+    Private Sub SPPuerto_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SPPuerto.DataReceived
+        Dim buffer As String = "Intervalo : " + "0.0" + " [seg]"
+        'buffer = SPPuerto.ReadExisting()
+        'txtRecibido.AppendText(buffer + vbNewLine)
+        Dim Tiempo As Double
+        Dim valor As Byte
+
+        valor = SPPuerto.ReadByte()
+        Tiempo += valor * 100
+        valor = SPPuerto.ReadByte()
+        Tiempo += valor * 10
+        valor = SPPuerto.ReadByte()
+        Tiempo += valor
+        valor = SPPuerto.ReadByte()
+        Tiempo += valor * 0.1
+        valor = SPPuerto.ReadByte()
+        Tiempo += valor * 0.01
+
+        Tiempo = Math.Round(Tiempo, 2)
+
+        Dim intervalo As Double = 0.0
+        If Not lstTiempos.Items.Count = 0 Then
+            intervalo = Math.Round((Tiempo - CDbl(lstTiempos.Items.Item(lstTiempos.Items.Count() - 1))), 2)
+            buffer = "intervalo : " + intervalo.ToString + " [seg]" + vbNewLine
+            lstTiempos.Items.Add(Tiempo)
+            txtRecibido.AppendText(buffer)
+        Else
+            buffer = "INICIO( 0.0 seg)" + vbNewLine
+            lstTiempos.Items.Add(0.0)
+            txtRecibido.AppendText(buffer)
+        End If
+
+
+
+
+
+        Dim archivo As System.IO.StreamWriter
+
+        Try
+
+            archivo = My.Computer.FileSystem.OpenTextFileWriter(txtArchivo.Text() + ".txt", True) 'modo permite definir si se sobrescribe o se anexa
+
+
+
+            archivo.WriteLine("marca = " + Tiempo.ToString() + "   ->  " + buffer)
+
+            archivo.Close()
+
+        Catch ex As Exception
+            MsgBox("falta ubicacion y nombre del archivo", MsgBoxStyle.Critical)
+        End Try
+
+        Dim N As Decimal = intervalo * 100
+        Math.Truncate(N)
+
+        Dim a_enviar() As Byte
+        ReDim a_enviar(3)
+
+        'convierto el nuemero doulble en sus digitos para enviarlos
+        a_enviar(0) = Int(N / 1000) 'Son las unidades de millar
+        a_enviar(1) = Int((N - a_enviar(0) * 1000) / 100) 'Las centenas igual pero restandole los millares
+        a_enviar(2) = Int((N - a_enviar(0) * 1000 - a_enviar(1) * 100) / 10) 'Las decenas igual pero restando los millares y centenas
+        a_enviar(3) = Int((N - a_enviar(0) * 1000 - a_enviar(1) * 100 - a_enviar(2) * 10) / 1) 'Las unidades restandole todo lo anterior. No haria falta dividir por 1, pero por estetica lo dejo.
+
+       
+        SPPuerto.Write(a_enviar(0))
+        SPPuerto.Write(a_enviar(1))
+        SPPuerto.Write(a_enviar(2))
+        SPPuerto.Write(a_enviar(3))
+
+    End Sub
+
+    
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+    End Sub
+End Class
